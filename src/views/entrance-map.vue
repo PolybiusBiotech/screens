c9f70e1289c5d63805cb45c42b84809d2d06637f
Marek Ventur <marekv@fb.com>
2022-05-15 17:11:51 -0700

    Add missing file. of course.

---
+++ src/views/entrance-map.vue
@@ -0,0 +1,37 @@
+<template>
+<layout>
+    <section id="map">
+        <Map warning="lower-tunnel,observation">
+            <ProfileMap />
+            <template #warning>Contamination Warning</template>
+        </Map>
+    </section>
+    </layout>
+</template>
+
+<style scoped>
+
+    :deep(#content) {
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr;
+        grid-template-areas:
+            "Map";
+    }
+
+    #map { grid-area: Map; }
+
+    #map {
+
+    }
+
+    #map{
+        overflow: hidden;
+    }
+
+</style>
+
+<script setup lang="ts">
+    import Layout from '@/layouts/primaryLayout.vue';
+    import Map from '@/widgets/map.vue';
+    import ProfileMap from '@/maps/profile.svg'
+</script>
