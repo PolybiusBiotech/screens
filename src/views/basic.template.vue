8751dfa3af9ed567253df6590ed3a6ef77809d49
Galia <github@galiaba.com>
2022-06-01 01:49:18 +0100

    Template to build pages from

---
+++ src/views/basic.template.vue
@@ -0,0 +1,29 @@
+<!-- for copypaste as basic template -->
+<!-- some suggestions in comments -->
+<template>
+	<layout>
+		
+
+		<!-- NewsMarquee id="news" /-->
+    </layout>
+</template>
+
+<style scoped>
+
+    /* :deep(#content) {
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 4vh;
+        grid-template-areas:
+            "X Y"
+            "News News";
+    }
+
+    #news { grid-area: News; } */
+
+
+</style>
+
+<script setup>
+    import Layout from '@/layouts/primaryLayout.vue';
+//    import Text from '@/widgets/text.vue';
+</script>
