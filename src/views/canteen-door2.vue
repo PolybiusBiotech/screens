d9a99c9198348177617a90e5c9d22271b697353a
Galia <github@galiaba.com>
2022-06-02 15:38:36 +0100

    Canteen door 2

---
+++ src/views/canteen-door2.vue
@@ -0,0 +1,25 @@
+<template>
+    <layout largeNotch>
+        <section id="map">
+            <Map current="life-support" warning="canteen,ia1,storage">
+                <TopDownMap />
+                <template #warning>Failure detected</template>
+            </Map>
+        </section>
+
+    </layout>
+</template>
+
+<style scoped>
+    :deep(#map) {
+        width: 90vw;
+    }
+
+</style>
+
+<script setup lang="ts">
+    import Layout from '@/layouts/primaryLayout.vue';
+    import Map from '@/widgets/map.vue';
+    import TopDownMap from '@/maps/top-down.svg'
+
+</script>
